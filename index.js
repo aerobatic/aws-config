8a49f3097647f8974db695b14a87e843aa01bcf6
David Von Lehman <david.vonlehman@gmail.com>
2016-02-29 13:31:36 -0800

    Incorporate DynamoDB TLS workaround agent options https://github.com/aws/aws-sdk-js/issues/862

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var AWS = require('aws-sdk');
+var url = require('url');
+var extend = require('extend');
+
+// Incorporate workaround agent settings to deal with Node/OpenSSL issue connecting to DynamoDB
+// https://github.com/aws/aws-sdk-js/issues/862
+var dynamoDbTlsAgentOptions = {
+  rejectUnauthorized: true,
+  keepAlive: true,                // workaround part i.
+  secureProtocol: 'TLSv1_method', // workaround part ii.
+  ciphers: 'ALL'                  // workaround part ii.
+};
 
 module.exports = function(options) {
   if (!options) options = {};
@@ -50,7 +61,11 @@ module.exports = function(options) {
   if (process.env.HTTPS_PROXY && options.sslEnabled !== false) {
     if (!awsOptions.httpOptions.agent) {
       var HttpsProxyAgent = require('https-proxy-agent');
-      awsOptions.httpOptions.agent = new HttpsProxyAgent(process.env.HTTPS_PROXY);
+      var proxyOpts = url.parse(process.env.HTTPS_PROXY);
+      if (options.dynamoTlsWorkaround) {
+        extend(proxyOpts, dynamoDbTlsAgentOptions);
+      }
+      awsOptions.httpOptions.agent = new HttpsProxyAgent(proxyOpts);
     }
   }
 
